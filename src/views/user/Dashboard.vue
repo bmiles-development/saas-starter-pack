b909e0a399cf9a6af869d6149843efd2cc58f85a
Michal Szymanski <michal.t.szymanski@gmail.com>
2020-09-10 22:19:40 +0200

    Updated locales files

--- src/views/user/Dashboard.vue
+++ src/views/user/Dashboard.vue
@@ -5,8 +5,6 @@
         <v-img class="my-3 logo" contain height="200" />
       </v-col>
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">{{WelcomeText}} {{AppTitle}}</h1>
-        <p class="subheading font-weight-regular">{{AppBiLine}}</p>
       </v-col>
     </v-row>
   </v-container>
@@ -15,15 +13,11 @@
 <script>
 // @ is an alias to /src
 import DashboardLayout from "@/layouts/DashboardLayout.vue";
-import { I18n } from "@aws-amplify/core";
+
 
 export default {
   name: "Home",
   data: () => ({
-    
-    WelcomeText: I18n.get("Welcome to"),
-    AppTitle: I18n.get("App"),
-    AppBiLine: I18n.get("The Ultimate App")
   }),
   created() {
     this.$emit("update:layout", DashboardLayout);
